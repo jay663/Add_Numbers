fcd56b52d685a942d8641cc4bab3955c19f34fe4
Jason Grucza <jay-g6@outlook.com>
2020-01-30 21:52:58 -0500

    Initial creation of add numbers api

---
+++ Add_Numbers/Controllers/AddingNumbersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Add_Numbers.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AddingNumbersController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<AddingNumbersController> _logger;
+
+        public AddingNumbersController(ILogger<AddingNumbersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public int Post(int number, int addend)
+        {
+            return number + addend;
+        }
+    }
+}
